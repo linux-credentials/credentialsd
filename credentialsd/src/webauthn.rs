f2fa3076af5ff98e96c3472aa447fe287df47f97
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2026-01-23 15:32:16 -0600

    Add Origin type

--- credentialsd/src/webauthn.rs
+++ credentialsd/src/webauthn.rs
@@ -690,4 +690,28 @@ pub fn format_client_data_json(
     };
     let cross_origin_str = if is_cross_origin { "true" } else { "false" };
     format!("{{\"type\":\"{op_str}\",\"challenge\":\"{challenge}\",\"origin\":\"{origin}\",\"crossOrigin\":{cross_origin_str}}}")
+
+#[derive(Debug)]
+pub(crate) enum Origin {
+    AppId(String),
+    SameOrigin(String),
+    CrossOrigin((String, String)),
+}
+
+impl Origin {
+    pub(crate) fn origin(&self) -> &str {
+        &match self {
+            Origin::AppId(app_id) => app_id,
+            Origin::SameOrigin(origin) => origin,
+            Origin::CrossOrigin((origin, _)) => origin,
+        }
+    }
+
+    pub(crate) fn top_origin(&self) -> Option<&str> {
+            Origin::AppId(_) => None,
+            Origin::SameOrigin(_) => None,
+            Origin::CrossOrigin((_, ref top_origin)) => Some(top_origin),
+        }
+    }
+}
 }
