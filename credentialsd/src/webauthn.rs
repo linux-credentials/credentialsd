4bfa369190f78a877967c6079efa35bbd625a154
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2026-01-23 15:32:16 -0600

    daemon: More strictly parse hostname from HTTPS origins

--- credentialsd/src/webauthn.rs
+++ credentialsd/src/webauthn.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Duration};
+use std::{collections::HashMap, fmt::Display, str::FromStr, time::Duration};
 
 use base64::{self, engine::general_purpose::URL_SAFE_NO_PAD, Engine};
 use libwebauthn::{
@@ -678,7 +678,11 @@ pub fn create_client_data_hash(json: &str) -> Vec<u8> {
         .to_owned()
 }
 
-pub fn format_client_data_json(op: Operation, challenge: &str, origin: &Origin) -> String {
+pub fn format_client_data_json(
+    op: Operation,
+    challenge: &str,
+    origin: &RequestEnvironment,
+) -> String {
     let op_str = match op {
         Operation::Create => "webauthn.create",
         Operation::Get => "webauthn.get",
@@ -699,41 +703,154 @@ pub fn format_client_data_json(op: Operation, challenge: &str, origin: &Origin) 
     client_data_json
 }
 
-#[derive(Debug)]
-pub(crate) enum Origin {
-    AppId(String),
-    SameOrigin(String),
-    CrossOrigin((String, String)),
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct AppId(String);
+
+impl AsRef<str> for AppId {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
 }
 
-impl Origin {
-    pub(crate) fn origin(&self) -> &str {
-        &match self {
-            Origin::AppId(app_id) => app_id,
-            Origin::SameOrigin(origin) => origin,
-            Origin::CrossOrigin((origin, _)) => origin,
+impl FromStr for AppId {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // begins with a letter
+        match s.chars().nth(0) {
+            Some(c) if c.is_ascii_alphabetic() => {}
+            _ => return Err(()),
+        };
+        // alphanumeric and labels separated by dots
+        if !s.chars().all(|c| c.is_ascii_alphanumeric() || c == '.') {
+            return Err(());
+        }
+        // ends with a valid label
+        if s.ends_with('.') {
+            return Err(());
+        }
+        Ok(AppId(s.to_string()))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub(crate) enum Origin {
+    Https { host: String, port: Option<u16> },
+    AppId(AppId),
+}
+
+impl Display for Origin {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Origin::Https { host, port } => {
+                write!(f, "https://{}", host)?;
+                if let Some(port) = port {
+                    write!(f, ":{port}")?;
+                }
+            }
+            Origin::AppId(app_id) => write!(f, "app:{}", app_id.0)?,
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for Origin {
+    type Err = OriginParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(rest) = s.strip_prefix("https://") {
+            let (host_candidate, port_candidate): (&str, Option<&str>) = rest
+                .split_once(':')
+                .map(|(h, p)| (h, Some(p)))
+                .unwrap_or((rest, None));
+
+            // begins with a letter
+            match host_candidate.chars().nth(0) {
+                Some(c) if c.is_ascii_alphabetic() => {}
+                _ => return Err(OriginParseError::InvalidHost),
+            };
+            // alphanumeric with hyphens and labels separated by dots
+            if !host_candidate
+                .chars()
+                .all(|c| c.is_ascii_alphanumeric() || c == '-' || c == '.')
+            {
+                return Err(OriginParseError::InvalidHost);
+            }
+            // ends with a valid label
+            if host_candidate.ends_with('.') {
+                return Err(OriginParseError::InvalidHost);
+            }
+            let host = host_candidate.to_ascii_lowercase();
+
+            let Ok(port) = port_candidate.map(|p| p.parse()).transpose() else {
+                return Err(OriginParseError::InvalidPort);
+            };
+
+            Ok(Origin::Https { host, port })
+        } else if let Some(app_id_candidate) = s.strip_prefix("app:") {
+            let app_id = app_id_candidate
+                .parse()
+                .map_err(|_| OriginParseError::InvalidHost)?;
+            Ok(Origin::AppId(app_id))
+        } else {
+            Err(OriginParseError::InvalidScheme)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum RequestEnvironment {
+    SameOrigin(Origin),
+    CrossOrigin((Origin, Origin)),
+}
+
+impl RequestEnvironment {
+    pub(crate) fn origin(&self) -> &Origin {
+        match self {
+            RequestEnvironment::SameOrigin(origin) => origin,
+            RequestEnvironment::CrossOrigin((origin, _)) => origin,
         }
     }
 
-    pub(crate) fn top_origin(&self) -> Option<&str> {
+    pub(crate) fn top_origin(&self) -> Option<&Origin> {
         match self {
-            Origin::AppId(_) => None,
-            Origin::SameOrigin(_) => None,
-            Origin::CrossOrigin((_, ref top_origin)) => Some(top_origin),
+            RequestEnvironment::SameOrigin(_) => None,
+            RequestEnvironment::CrossOrigin((_, ref top_origin)) => Some(top_origin),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum OriginParseError {
+    InvalidScheme,
+    InvalidHost,
+    InvalidPort,
+}
+
+impl std::error::Error for OriginParseError {}
+
+impl Display for OriginParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidScheme => f.write_str("Invalid scheme"),
+            Self::InvalidHost => f.write_str("Invalid host"),
+            Self::InvalidPort => f.write_str("Invalid port"),
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{format_client_data_json, Operation, Origin};
+    use crate::webauthn::{Origin, OriginParseError};
+
+    use super::{format_client_data_json, Operation, RequestEnvironment};
     #[test]
     fn test_same_origin_client_data_json_str() {
         let expected = r#"{"type":"webauthn.create","challenge":"abcd","origin":"https://example.com","crossOrigin":false}"#;
         let json = format_client_data_json(
             Operation::Create,
             "abcd",
-            &Origin::SameOrigin("https://example.com".to_string()),
+            &RequestEnvironment::SameOrigin("https://example.com".parse().unwrap()),
         );
         assert_eq!(expected, json);
     }
@@ -744,11 +861,61 @@ mod tests {
         let json = format_client_data_json(
             Operation::Create,
             "abcd",
-            &Origin::CrossOrigin((
-                "https://example.com".to_string(),
-                "https://example.org".to_string(),
+            &RequestEnvironment::CrossOrigin((
+                "https://example.com".parse().unwrap(),
+                "https://example.org".parse().unwrap(),
             )),
         );
         assert_eq!(expected, json);
+    }
+
+    fn check_https_origin(origin: &str, expected_host: &str, expected_port: Option<u16>) {
+        let Origin::Https { host, port }: Origin = origin.parse().unwrap() else {
+            panic!("Not an https origin");
+        };
+        assert_eq!(expected_host, host);
+        assert_eq!(expected_port, port);
+    }
+
+    #[test]
+    fn test_origin_parse_when_http_fails() {
+        let err = "http://example.com".parse::<Origin>().unwrap_err();
+        assert!(matches!(err, OriginParseError::InvalidScheme));
+    }
+
+    #[test]
+    fn test_origin_parse_https_origin_without_port_succeeds() {
+        check_https_origin("https://example.com", "example.com", None);
+    }
+
+    #[test]
+    fn test_origin_parse_https_with_port_succeeds() {
+        check_https_origin("https://example.org:8443", "example.org", Some(8443));
+    }
+
+    #[test]
+    fn test_origin_parse_with_trailing_slash_fails() {
+        let err = "https://example.org/".parse::<Origin>().unwrap_err();
+        assert!(matches!(err, OriginParseError::InvalidHost));
+    }
+
+    #[test]
+    fn test_origin_parse_with_port_and_path_fails() {
+        let err = "https://example.org:8443/".parse::<Origin>().unwrap_err();
+        assert!(matches!(err, OriginParseError::InvalidPort));
+    }
+
+    #[test]
+    fn test_origin_parse_with_invalid_characters_fails() {
+        let err = "https://😭.edu:1234".parse::<Origin>().unwrap_err();
+        assert!(matches!(err, OriginParseError::InvalidHost));
+    }
+
+    #[test]
+    fn test_origin_parse_app_id_succeeds() {
+        let Origin::AppId(app_id) = "app:com.example.ExampleApp".parse::<Origin>().unwrap() else {
+            panic!("not an app origin");
+        };
+        assert_eq!("com.example.ExampleApp", app_id.0);
     }
 }
