583c8347b7f2d9978ef7e8b26393d1fb50b079da
Isaiah Inuwa <isaiah.inuwa@gmail.com>
2026-06-21 19:35:32 -0500

    daemon: Delete outdated tests

--- credentialsd/src/dbus/mod.rs
+++ credentialsd/src/dbus/mod.rs
@@ -18,9 +18,3 @@ pub use self::{
     },
     ui_control::UiControlServiceClient,
 };
-
-#[cfg(test)]
-pub mod test {
-    pub use super::flow_control::test::DummyFlowServer;
-    pub use super::ui_control::test::DummyUiServer;
-}
